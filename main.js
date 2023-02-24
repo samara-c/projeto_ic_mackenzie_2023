7fd00c279e676299627f05c682c597ce9774ad76
Samara <41492804+samara-c@users.noreply.github.com>
2023-02-24 00:22:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,98 @@
-import { Player } from "./player.js";
+
 
 window.addEventListener('load', function(){
 
     //variaveis de configuracao da tela
 
     const screen = document.querySelector('canvas');
+    const texto_jogo = document.querySelector('.titulo-principal')
     const ctx = screen.getContext('2d');
     screen.width = 900;
     screen.height = 600;
     var recuo_tela = 0;
 
-    class Game{
+    // variaveis personagens
+    const pasta_personagens = 'img/characters/'
+    const pasta_bgs = 'img/bgs/'
 
-        constructor(width, height){
-            this.width = width;
-            this.height = height;
-            this.player = new Player(this);
-        }
-
-        update(){
-
-        }
-
-        draw(context){
-            this.player.draw(context);
+    
+    var pos_x_personagem = 10;
+    var pos_y_personagem = 130;
 
 
-        }
-
-        animate(){
-            ctx.clearRect(0,0, screen.width, screen.height);
-            game.draw(ctx);
-            requestAnimationFrame(animate);
-
-        }
-        animate();
+    function desenhaTelaPadrao(){
+        ctx.fillStyle = 'black';
+        ctx.fillRect(recuo_tela, recuo_tela, screen.width, screen.height);
+        desenhaBG('school');
+        desenhaCaixaDeTexto();
+        defineEDesenhaPersonagem("julia","happy","d");
     }
-    const game = new Game(screen.width, screen.height);
-    console.log(game);
 
+    // valores para personagem: 'julia', 'pedro', 'otavio'
+    // valores para sentimento: 'angry', 'happy', 'sad'
+    function defineEDesenhaPersonagem(personagem, sentimento, posicao = "e") {
+
+        var personagem_default = new Image();
+        var tamanho_personagem_x = 250;
+        var tamanho_personagem_y = 553;
+
+        // coloca o personagem do outro lado da tela se o valor recebido for 'e' (esquerda)
+        if (posicao=="e"){
+            pos_x_personagem = 640;
+        }
+        
+        var caminho_personagem = pasta_personagens+personagem+"_"+sentimento+"_"+posicao+".png";
+        personagem_default.src=caminho_personagem;
+        console.log(caminho_personagem);
+
+        ctx.drawImage(personagem_default,pos_x_personagem,pos_y_personagem,
+           tamanho_personagem_x,tamanho_personagem_y);
+
+    }
+
+    function desenhaCaixaDeTexto(){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(10, 10, 880, 200);
+        ctx.font = '25px arial';
+        ctx.fillStyle = 'black';
+        ctx.fillText('Ola mundo', 30,70);
+
+    }
+
+    function desenhaBG(ambiente){
+        var bg_default = new Image();
+        var caminho_bg = pasta_bgs+ambiente+".png";
+        bg_default.src=caminho_bg;
+
+        ctx.drawImage(bg_default,0,0,screen.width,screen.height);
+
+
+    }
+    
+
+    function iniciaJogo(inicia){
+        if (inicia){
+            desenhaTelaPadrao();
+
+
+        }
+    }
+
+    function retornaPosicaoClique(evento){
+
+        var pos_x = evento.pageX - screen.offsetLeft;
+        var pos_y = evento.pageY - screen.offsetTop;
+        var vetor_pos_mouse = [pos_x,pos_y];
+
+        texto_jogo.innerHTML=vetor_pos_mouse;
+        return vetor_pos_mouse;
+
+        
+          
+    }
+
+
+    
+    screen.onclick = retornaPosicaoClique;
+    iniciaJogo(true);
 })
-
-
-
-//variaveis de arquivos externos (imagens)
-
-var fundo_tela_inicial_img = new Image();
